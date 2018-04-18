c646f072ca4d19e800e5b5360cf6096ab338c559
Duy Long <dangduylong96@gmail.com>
2018-04-18 21:10:07 +0700

    Tao bang company va them du lieu cho bang

--- app/Company.php
+++ app/Company.php
@@ -6,6 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    protected $table='companies';
-    public $timestamps = false;
+    protected $table='company';
 }
---
+++ database/migrations/2018_04_18_204653_create_table_company.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCompany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('size');
+            $table->text('address');
+            $table->integer('place');
+            $table->text('image');
+            $table->text('desc');
+            $table->string('website');
+            $table->text('phone');
+            $table->integer('view');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
