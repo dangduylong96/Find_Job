511328759deb45b57f4e4108e8c77b67ea07b170
Duy Long <dangduylong96@gmail.com>
2018-04-18 20:38:14 +0700

    Tao bang va du lieu user thanh cong

--- database/seeds/Users.php
+++ database/seeds/Users.php
@@ -11,13 +11,21 @@ class Users extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'type'      =>  'admin',
-            'email'     =>  'dangduylong96@gmail.com',
-            'password'  =>  Hash::make('bongma00'),
-            'created_at' => new DateTime,
-            'updated_at' => new DateTime,
-            'status'    =>  1
-        ]);
+        //DÁN VÔ SQL MÀ CHẠY
+
+        // INSERT INTO `users` (`id`, `type`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `status`) VALUES
+        // (1, 'admin', 'dangduylong96@gmail.com', '$2y$10$qRGfdzyM3WvZixLSw9ijuu6geyhDPIsCT/jnaWYGUPZ/bd0pjZzoG', 'wDhx5jVrPHL0tEIW6Ozsb0VgBtHnBjsfwCW7Es1dcZARqHpEJ7fT83iko8ES', '2018-03-10 06:21:09', '2018-03-10 06:21:09', 1),
+        // (2, 'employer', 'nhatuyendung@gmail.com', '$2y$10$Ym6zJFvlvimx7.Ryid4NBuIanPKJ0rZMfSqsJQA0gW7xcix0pfh5K', 'e8FVeLDV3fEIIQMH6YssSKMliUvWtA2FGxwqBWPahrdQ4T7wHwjrx3o8HH9A', '2018-03-10 14:43:41', '2018-03-10 14:43:41', 1),
+        // (3, 'employer', 'nhatuyendung1@gmail.com', '$2y$10$kW/d8sTx7Oyq2cy7I3UX4uwf/HrX7zKL/hw0UKVw536T/YpCyEm0q', '799fQtBJgCLUw5pesOcEfP5lAFl4W2Aon4OfN2fk04FIQ2KyLoJAXdQuSaTF', '2018-03-10 14:44:24', '2018-03-10 14:44:24', 1),
+        // (4, 'employer', 'nhatuyendung5@gmail.com', '$2y$10$KI1Tr0BeqkEytIdYeeuPA.9Qzrgmeyxdxsk82kSqXFGFT6Z/OWmr2', NULL, '2018-03-10 14:47:03', '2018-03-10 14:47:03', 1),
+        // (5, 'candidate', 'ungvien@gmail.com', '$2y$10$1J50.6KuuyRdNplAZ.yM4ePgHy9fmceBLnWoL90blJjgLXx044yPi', 'CNoJyR08KoJolgN87dqMKGpA3sCGN7UUhHtDupbdqaDuIa7uiYp7Te48Ymye', '2018-03-14 03:46:28', '2018-03-14 03:46:28', 1),
+        // (6, 'candidate', 'ungvien2@gmail.com', '$2y$10$Ew3nfqEvBnb7ZVjtZ2M3X.ZrwnSkRL1lYBSF0H88WJXccJjEhjDG.', NULL, '2018-03-14 03:48:08', '2018-03-14 03:48:08', 1),
+        // (7, 'candidate', 'ungvien3@gmail.com', '$2y$10$hYP6Kr6EixnKlKlCymPVsOxnhvYDgee.jdnLSqnyHkNnmGpxPfbli', NULL, '2018-03-14 03:49:19', '2018-03-14 03:49:19', 1),
+        // (8, 'candidate', 'ungvien4@gmail.com', '$2y$10$ZD8Lyv0nJFJhTMwluS79Ruou5DFc71Ax.JBg1B6f/3EzTPJcOs6Ty', NULL, '2018-03-14 03:51:22', '2018-03-14 03:51:22', 1),
+        // (9, 'employer', 'ntd@gmail.com', '$2y$10$eAF2AL6sPjgtpX4OFSbsSupTnB6eoEkzjqdE7gOPTzuktVSVFGlee', '4zMgaxvnMnDpcGDOXzoU0m8HqtWrQ9n9SwfF6jBGiI4GupLFlmnOGRQUppdK', '2018-03-18 01:51:26', '2018-03-18 01:51:26', 1),
+        // (10, 'employer', 'ntd1@gmail.com', '$2y$10$fTI9bDI0EeGCTwdNiVC2VO0FpzfIaRTqbnxW7Lc98Vz9SifWZm.1e', 'UdbU9LpnoN4koJDfRFkW35h4MIhOkVWC2i7ORsUnOwiAcbrh2Hcy7QitQ9bg', '2018-03-18 02:03:01', '2018-03-18 02:03:01', 1),
+        // (11, 'employer', 'ntd2@gmail.com', '$2y$10$SCUx0Sfu9icjUGni2pOeIOl4g70k18NA86XYPjRth2fUVupFwKrTy', 'MNSIeZFuLvZ4rgSSuTzwlpe1PYHoi8gOl0MF5Vr24NWlzKc1JQt2HjkWdorR', '2018-03-18 02:13:06', '2018-03-18 02:13:06', 1),
+        // (12, 'candidate', 'uv@gmail.com', '$2y$10$DvXw7EYQMgcvoHvwpb8hhO706RlBIL0iVMUCdc7cfq6/PbgP4vobG', NULL, '2018-03-19 04:04:44', '2018-03-19 04:04:44', 1),
+        // (13, 'employer', 'nh0xpr0py2@gmail.com', '$2y$10$FvmwRLKWId0SlKKuTc2AQunVQOUjq7Hg7aTKdAiXEboINBCUPZ.ce', NULL, '2018-03-21 13:35:21', '2018-03-21 13:35:21', 1);
     }
 }
